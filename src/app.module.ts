e5ad4e01f7243f3c29eefc0d6febd8ba36e0c1c9
bichipham <ptbich308@gmail.com>
2025-10-29 15:31:11 +0700

    + add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomModule } from './modules/modules-api/room/room.module';
-
+import { PrismaModule } from './modules/modules-system/prisma/prisma.module';
 @Module({
-  imports: [RoomModule],
+  imports: [RoomModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/modules-api/room/room.service.ts
+++ src/modules/modules-api/room/room.service.ts
@@ -1,8 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/modules/modules-system/prisma/prisma.service';
 
 @Injectable()
 export class RoomService {
-  findAll() {
-    return ['Room 1', 'Room 2', 'Room 3'];
-  }
+    constructor(private readonly prisma: PrismaService) { }
+    async findAll() {
+        const rooms = await this.prisma.rooms.findMany();
+        return rooms;
+    }
 }
